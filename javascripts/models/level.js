eee1a1183c0686243e2576189cea35450c669545
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-05 15:38:34 -0700

    updated level.js and teacher.js constructors

--- javascripts/models/level.js
+++ javascripts/models/level.js
@@ -1,12 +1,18 @@
 class Level {
     static all = []
 
-    constructor(id, title, dance_classes = []) {
-        this.id = id
-        this.title = title
-        this.dance_classes = dance_classes
-        Level.all.push(this)
+    constructor(levelObj) {
+        this.id = levelObj.id
+        this.title = levelObj.attributes.title
+        this.dance_classes = levelObj.attributes.dance_classes
     }
+
+    // constructor(id, title, dance_classes = []) {
+    //     this.id = id
+    //     this.title = title
+    //     this.dance_classes = dance_classes
+    //     Level.all.push(this)
+    // }
 
     static getAll = () => {
         this.all
--- javascripts/models/teacher.js
+++ javascripts/models/teacher.js
@@ -1,13 +1,19 @@
 class Teacher {
     static all = []
 
-    constructor(id, name, bio, dance_classes = []) {
-        this.id = id
-        this.name = name
-        this.bio = bio
-        this.dance_classes = dance_classes
-        Teacher.all.push(this)
+    constructor(teacherObj) {
+        this.id = teacherObj.id
+        this.name = teacherObj.attributes.name
+        this.bio = teacherObj.attributes.bio
     }
+
+    // constructor(id, name, bio, dance_classes = []) {
+    //     this.id = id
+    //     this.name = name
+    //     this.bio = bio
+    //     this.dance_classes = dance_classes
+    //     Teacher.all.push(this)
+    // }
 
     static getAll = () => {
         this.all
